56dfc7c4645619d10086d61153950aedf0995cc1
Eugene Ha <eha@posteo.de>
2018-05-14 20:17:26 +0200

    rlang issues closed

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -15,12 +15,7 @@ expect_all_equal <- expectations(expect_equal)
 
 expect_all_identical <- expectations(expect_identical)
 
-# As of testthat 1.0.2.9000, objects are captured as quosures by `expect_*()`.
-# This causes spurious test failures, due to a bug in `rlang::enquo()`
-# (https://github.com/tidyverse/rlang/issues/280) and its inability to
-# comprehend literal unquoting operators
-# (cf. https://github.com/tidyverse/rlang/issues/264).
-
+# Pre-empt interpretation of unquoting operators
 expect_equal_ <- function(object, expected, ...) {
   force(object)
   force(expected)
