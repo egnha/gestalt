dd27172b197d49f82c62858656b378e46bc3d40f
Eugene Ha <eha@posteo.de>
2018-07-26 12:49:43 -0400

    Appease spurious 'R CMD check' note

--- R/compose.R
+++ R/compose.R
@@ -460,7 +460,7 @@ lambda_named <- function(expr, env) {
 is_lambda <- check_head("{")
 
 lambda <- function(body, env) {
-  new_fn(alist(... = , . = ..1), body, env)
+  new_fn(list(... = quote(expr = ), . = quote(..1)), body, env)
 }
 
 lambda_partial <- local({
